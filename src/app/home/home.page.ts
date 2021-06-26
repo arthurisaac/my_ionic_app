415c1de5163d12866ea8b14c4b1a28a02285d0be
Arthur Sondo <r.thur.light@gmail.com>
2021-06-26 19:28:30 +0000

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  posts = [];
 
-  constructor() {}
+  constructor(public http: HttpClient) {}
 
+  ngOnInit() {
+    this.recuperationDonnees();
+  }
+
+  async recuperationDonnees() {
+    console.log('recupération des données');
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    await this.http.get<any>(url)
+      .subscribe(
+        res => {
+          console.log(res);
+          this.posts = res;
+        },
+        error => {
+          console.error(error);
+        }
+      );
+  }
 }
